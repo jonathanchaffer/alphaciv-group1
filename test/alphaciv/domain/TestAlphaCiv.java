f1c47176d21b7b8bee623bf7cc88de7ecc48a458
jonathanchaffer <jonathanchaffer@gmail.com>
2020-09-03 08:55:43 -0400

    add shouldBeRedTurnFirst test

--- test/alphaciv/domain/TestAlphaCiv.java
+++ test/alphaciv/domain/TestAlphaCiv.java
@@ -3,39 +3,37 @@ package alphaciv.domain;
 import org.junit.*;
 import static org.junit.Assert.*;
 
-/** Skeleton class for AlphaCiv test cases 
-
-   This source code is from the book 
-     "Flexible, Reliable Software:
-       Using Patterns and Agile Development"
-     published 2010 by CRC Press.
-   Author: 
-     Henrik B Christensen 
-     Computer Science Department
-     Aarhus University
-   
-   This source code is provided WITHOUT ANY WARRANTY either 
-   expressed or implied. You may study, use, modify, and 
-   distribute it for non-commercial purposes. For any 
-   commercial use, see http://www.baerbak.com/
-*/
+/**
+ * Skeleton class for AlphaCiv test cases
+ * 
+ * This source code is from the book "Flexible, Reliable Software: Using
+ * Patterns and Agile Development" published 2010 by CRC Press. Author: Henrik B
+ * Christensen Computer Science Department Aarhus University
+ * 
+ * This source code is provided WITHOUT ANY WARRANTY either expressed or
+ * implied. You may study, use, modify, and distribute it for non-commercial
+ * purposes. For any commercial use, see http://www.baerbak.com/
+ */
 public class TestAlphaCiv {
-  private Game game;
-  /** Fixture for alphaciv testing. */
-  @Before
-  public void setUp() {
-    game = new GameImpl();
-  }
+	private Game game;
 
-  @Test
-  public void shouldHaveRedCityAt1_1() {
-    City c = game.getCityAt(new Position(1,1));
-    assertNotNull("There should be a city at (1,1)", c);
-    Player p = c.getOwner();
-    assertEquals( "City at (1,1) should be owned by red",
-      Player.RED, p );
-  }
- // @Test
-  
-  
+	/** Fixture for alphaciv testing. */
+	@Before
+	public void setUp() {
+		game = new GameImpl();
+	}
+
+	@Test
+	public void shouldHaveRedCityAt1_1() {
+		City c = game.getCityAt(new Position(1, 1));
+		assertNotNull("There should be a city at (1,1)", c);
+		Player p = c.getOwner();
+		assertEquals("City at (1,1) should be owned by red", Player.RED, p);
+	}
+	
+	@Test
+	public void shouldBeRedTurnFirst() {
+		assertEquals(Player.RED, game.getPlayerInTurn());
+	}
+
 }
