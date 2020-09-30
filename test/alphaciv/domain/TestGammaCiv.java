8d38b5c585844017b18a59be73916e447c7d8390
jonathanchaffer <jonathanchaffer@gmail.com>
2020-09-30 11:05:13 -0400

    add blueCannotPerformRedUnitAction test

--- test/alphaciv/domain/TestGammaCiv.java
+++ test/alphaciv/domain/TestGammaCiv.java
@@ -9,24 +9,36 @@ import org.junit.Test;
 
 public class TestGammaCiv {
 	private Game game;
-	
+
 	@Before
 	public void setUp() {
 		game = new GammaGameFactory().createGame();
 	}
-	
+
 	@Test
 	public void settlerPerformsActionandBuildsCity() {
-		Unit settler = game.getUnitAt(new Position (4,3));
+		Unit settler = game.getUnitAt(new Position(4, 3));
 		assertNotNull(settler);
 		assertEquals(Player.RED, settler.getOwner());
 		assertEquals(GameConstants.SETTLER, settler.getTypeString());
-		game.performUnitActionAt(new Position(4,3));
-		assertNull(game.getUnitAt(new Position(4,3)));
-		City newCity = game.getCityAt(new Position(4,3));
+		game.performUnitActionAt(new Position(4, 3));
+		assertNull(game.getUnitAt(new Position(4, 3)));
+		City newCity = game.getCityAt(new Position(4, 3));
 		assertNotNull(newCity);
 		assertEquals(Player.RED, newCity.getOwner());
 		assertEquals(1, newCity.getSize());
 	}
-	
+
+	@Test
+	public void blueCannotPerformRedUnitAction() {
+		game.endOfTurn();
+		Unit settler = game.getUnitAt(new Position(4, 3));
+		assertNotNull(settler);
+		assertEquals(Player.RED, settler.getOwner());
+		assertEquals(GameConstants.SETTLER, settler.getTypeString());
+		game.performUnitActionAt(new Position(4, 3));
+		assertNotNull(game.getUnitAt(new Position(4, 3)));
+		assertNull(game.getCityAt(new Position(4, 3)));
+	}
+
 }
