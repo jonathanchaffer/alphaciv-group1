2dc85ef4735264d7a976742dd0b04b88f34fe2e4
carmenrl <carmen.rodriguezlob@hope.edu>
2020-09-30 20:39:33 -0400

    Added Unit tests for GammaUnitActionStrategy, GammaUnitMovementStrategy, AlphaUnitMovementStrategy

---
+++ test/alphaciv/domain/TestAlphaUnitMovementStrategy.java
@@ -0,0 +1,21 @@
+package alphaciv.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestAlphaUnitMovementStrategy {
+	UnitMovementStrategy movementStrategy;
+	
+	@Before
+	public void setUp() {
+		movementStrategy = new AlphaUnitMovementStrategy();
+	}
+	@Test
+	public void testCanMoveUnit() {
+		Unit unit = new UnitImpl(GameConstants.ARCHER, Player.RED);
+		assertTrue(movementStrategy.canMoveUnit(unit));
+	}
+
+}
---
+++ test/alphaciv/domain/TestGammaUnitMovementStrategy.java
@@ -0,0 +1,28 @@
+package alphaciv.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestGammaUnitMovementStrategy {
+
+	UnitMovementStrategy movementStrategy;
+
+	@Before
+	public void setUp() {
+		movementStrategy = new GammaUnitMovementStrategy();
+	}
+	@Test
+	public void testCanMoveUnit() {
+		Unit unit = new UnitImpl(GameConstants.ARCHER, Player.RED);
+		assertTrue(movementStrategy.canMoveUnit(unit));
+	}
+	@Test
+	public void cannotMoveFortifiedUnit() {
+		Unit unit = new UnitImpl(GameConstants.ARCHER, Player.RED);
+		((UnitImpl) unit).toggleIsFortified();
+		assertFalse(movementStrategy.canMoveUnit(unit));
+	}
+
+}
