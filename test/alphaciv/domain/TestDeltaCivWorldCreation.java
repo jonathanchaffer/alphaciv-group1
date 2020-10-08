7ea4a055dafe9d31c459c091ac8b76ceaab1443b
jonathanchaffer <jonathanchaffer@gmail.com>
2020-10-08 09:04:46 -0400

    add missing import

--- test/alphaciv/domain/TestDeltaCivWorldCreation.java
+++ test/alphaciv/domain/TestDeltaCivWorldCreation.java
@@ -2,6 +2,7 @@ package alphaciv.domain;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
 
